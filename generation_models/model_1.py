63357b1baa585ce45e91a19398e89387bd89b408
pUrGe12 <helloitsme0218@gmail.com>
2024-11-27 00:31:57 +0530

    changed name

--- generation_models/model_1.py
+++ generation_models/model_1.py
@@ -9,12 +9,9 @@ from pathlib import Path
 import re
 from supabase import create_client, Client                                        # For database adding and pulling
 
+NAME = "model_1"
+
 load_dotenv(dotenv_path=Path(__file__).parent.parent / '.env')
-
-url: str = str(os.getenv("SUPABASE_URL")).strip()
-key: str = str(os.getenv("SUPABASE_KEY")).strip()
-
-supabase: Client = create_client(url, key)
 
 API_KEY = str(os.getenv("API_KEY")).strip()
 
@@ -22,3 +19,4 @@ genai.configure(api_key = API_KEY)
 
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
+
