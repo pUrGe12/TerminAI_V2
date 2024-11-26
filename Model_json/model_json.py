929ce4bab37fe1822967afe951ae01f1f334e76a
pUrGe12 <helloitsme0218@gmail.com>
2024-11-26 13:42:51 +0530

    implemented history

--- Model_json/model_json.py
+++ Model_json/model_json.py
@@ -5,12 +5,20 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 import re
+from supabase import create_client, Client                                        # For database adding and pulling
+
 
 NAME = "model_json"
 
 # Load the API keys from environment
 
 load_dotenv(dotenv_path=Path(__file__).parent.parent / '.env')
+
+url: str = str(os.getenv("SUPABASE_URL")).strip()
+key: str = str(os.getenv("SUPABASE_KEY")).strip()
+
+supabase: Client = create_client(url, key)
+
 API_KEY = str(os.getenv("API_KEY")).strip()
 
 genai.configure(api_key=API_KEY)
@@ -18,9 +26,26 @@ genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
 
+
+def get_history(n):
+    """
+    This function queries supabase and gets the last n data points, and returns a list of the required parameters, after filtering it. This is done from version 2.
+
+    n -> the number of entries you want. I have kept it to be usually at 5.
+    """
+    response = supabase.table('History_v2').select("*").order('id', desc=True).limit(n).execute().data      # Take the last n
+    
+    keys_to_keep = {'system_boolean', 'ex_model_function', 'user_prompt', 'ex_work_summary'}
+    filtered_data = [{key: dos[key] for key in dos if key in keys_to_keep} for dos in response]    
+    return response
+
+
 def GPT_response(user_prompt):
+    history = get_history(3)
     prompt = prompts.get(NAME).strip() + f"""
+                    This is the history: {history}
                     This is the user's prompt: {user_prompt}
+
                 """
     try:
         output = ''
