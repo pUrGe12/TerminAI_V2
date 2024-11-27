496459008bc99a225c6f643bcaf113273c1835e2
pUrGe12 <helloitsme0218@gmail.com>
2024-11-27 22:58:12 +0530

    tested parser

--- test/json_parsing/parser.py
+++ test/json_parsing/parser.py
@@ -1,5 +1,7 @@
 import json
 
+prompt = """connect to wifi "motog73" and start wireshark recording"""
+
 test_js = """ 
 [
 	{ 
@@ -20,7 +22,15 @@ operations = json.loads(test_js)
 
 sorted_operations = sorted(operations, key=lambda x: x['order']) 			# in general we can assume it will give us the sorted output only
 
-
 for op in sorted_operations:
+	''' We've ensured that the parameter will be "operation" itself and not "operation 1" or something'''
 	a = op['operation'].replace('_', ' ') 			# In case we need to, will not give errors if none found
 	print(a)
+
+
+''' 
+The next step is classifying them into one of the 6 categories and then calling the right function. We will prolly have to use a LLM here as well.
+
+Alternatively, we can tell the main guy to classify it for us... Ponder...
+
+'''
