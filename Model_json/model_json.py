6f12a3e0debc06d84227cbc11f9806c3fc77fbab
pUrGe12 <helloitsme0218@gmail.com>
2024-11-28 19:30:55 +0530

    updated model_json

--- Model_json/model_json.py
+++ Model_json/model_json.py
@@ -12,17 +12,16 @@ from pathlib import Path
 import re
 from supabase import create_client, Client                                        # For database adding and pulling
 
-
 NAME = "model_json"
 
 # Load the API keys from environment
 
 load_dotenv(dotenv_path=Path(__file__).parent.parent / '.env')
 
-url: str = str(os.getenv("SUPABASE_URL")).strip()
-key: str = str(os.getenv("SUPABASE_KEY")).strip()
+# url: str = str(os.getenv("SUPABASE_URL")).strip()
+# key: str = str(os.getenv("SUPABASE_KEY")).strip()
 
-supabase: Client = create_client(url, key)
+# supabase: Client = create_client(url, key)
 
 API_KEY = str(os.getenv("API_KEY")).strip()
 
@@ -30,20 +29,6 @@ genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel('gemini-pro')
 chat = model.start_chat(history=[])
-
-
-# def get_history(n):
-#     """
-#     This function queries supabase and gets the last n data points, and returns a list of the required parameters, after filtering it. This is done from version 2.
-
-#     n -> the number of entries you want. I have kept it to be usually at 5.
-#     """
-#     response = supabase.table('History_v2').select("*").order('id', desc=True).limit(n).execute().data      # Take the last n
-    
-#     keys_to_keep = {'system_boolean', 'ex_model_function', 'user_prompt', 'ex_work_summary'}
-#     filtered_data = [{key: dos[key] for key in dos if key in keys_to_keep} for dos in response]    
-#     return response
-
 
 def GPT_response(user_prompt):
     # history = get_history(3)
@@ -71,4 +56,4 @@ def GPT_response(user_prompt):
 
     except Exception as e:
         print(f"Error generating GPT response in model_json: {e}")
-        return 'YAHA HUI HAI GADBAD'
+        return 'Try again'
