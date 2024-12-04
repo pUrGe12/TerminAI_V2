8047001cf1d13f5626ec04af7370024f339f71fc
pUrGe12 <helloitsme0218@gmail.com>
2024-12-04 23:38:10 +0530

    added sample data.py

---
+++ utils/setup/data.py
@@ -0,0 +1,7 @@
+# If the setup fails, enter your information here
+
+user_dict = {
+    "username": "",
+    "operating_system": "",
+    "sudo_password": ""             # If you are not going to run super admin commands, then enter REDACTED
+}
