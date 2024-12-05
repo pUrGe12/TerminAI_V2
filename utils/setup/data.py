8c9897979c060255846b071208f4a20ee7cf7f0e
pUrGe12 <helloitsme0218@gmail.com>
2024-12-05 17:34:56 +0530

    created command finder code

--- utils/setup/data.py
+++ utils/setup/data.py
@@ -1,7 +1,5 @@
-# If the setup fails, enter your information here
-
 user_dict = {
     "username": "purge",
     "operating_system": "Linux",
-    "sudo_password": "REDACTED"             # If you are not going to run super admin commands, then enter REDACTED
+    "sudo_password": "REDACTED"
 }
--- utils/setup/setup.sh
+++ utils/setup/setup.sh
@@ -32,4 +32,23 @@ SUPABASE_URL=$supabase_url
 SUPABASE_KEY=$supabase_key
 EOF
 
+# Also adding the command finder code here to figure out which commands are available in the system
+
+# I am using compgen to find the commands present in the system that can be executed, excluding those starting with "_" cause those can be called
+# from completion functions only (don't know what that means but anyway)
+
+commands=$(compgen -c | grep -v '^_' | sort -u)             # Sorting and removing duplicates as well
+
+output_file="system_commands.py"                            # saving that to a python file cause its easier to import a list
+
+{
+  echo "commands_list = ["
+  for cmd in $commands; do
+    echo "    \"$cmd\","
+  done
+  echo "]"
+} > $output_file
+
+# Creating a list and saving the commands. 
+
 echo "Setup completed. Ensure that you have run the table_creation.sql command in the SQL editor of your database"
